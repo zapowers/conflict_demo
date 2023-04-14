e332cc429d155e8f9bca0f18272e8d3f276c873b
Zachary Powers <zachapowers@gmail.com>
2023-04-14 12:43:02 -0500

    add city

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,17 @@
-def hello_world():
-    print(f'Hello World!')
+def hello_world(city):
+    print(f'Hello World {city}!')
 
 
 def main():
-    hello_world()
+    hello_world('NYC')
+
+
+main()def hello_world(city):
+    print(f'Hello World {city}!')
+
+
+def main():
+    hello_world('NYC')
 
 
 main()
